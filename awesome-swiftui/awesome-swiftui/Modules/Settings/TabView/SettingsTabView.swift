cbda13775694f63b7dffc245c7ff82d5aa1b2878
Huy D <116730653+mjn2max@users.noreply.github.com>
2025-08-01 14:25:15 -0400

    Update SettingsTabView

--- awesome-swiftui/awesome-swiftui/Modules/Settings/TabView/SettingsTabView.swift
+++ awesome-swiftui/awesome-swiftui/Modules/Settings/TabView/SettingsTabView.swift
@@ -12,31 +12,35 @@
 import SwiftUI
 
 struct SettingsTabView: View {
-    NavigationStack {
-        List {
-            ForEach(SettingSection.allCases, id: \.self) { section in
-                Section(section.title) {
-                    let items = viewModel.settingList.filter { $0.section == section }
-                    ForEach(items) { item in
-                        switch item.data {
-                        case SettingGeneral.colorScheme:
-                            General_ColorSchemeView(sfSymbol: item.data.sfSymbol, titleText: item.data.title)
-                        case SettingGeneral.accentColor:
-                            NavigationLink(value: item) {
-                                ItemRowView(sfSymbol: item.data.sfSymbol, titleText: item.data.title, infoText: themeManager.accentColor.toHex())
-                            }
-                        default:
-                            NavigationLink(value: item) {
-                                ItemRowView(sfSymbol: item.data.sfSymbol, titleText: item.data.title, infoText: "Demo")
+    @ObservedObject private var viewModel: SettingsViewModel = .init()
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(SettingSection.allCases, id: \.self) { section in
+                    Section(section.title) {
+                        let items = viewModel.settingList.filter { $0.section == section }
+                        ForEach(items) { item in
+                            switch item.data {
+                            case SettingGeneral.colorScheme:
+                                General_ColorSchemeView(sfSymbol: item.data.sfSymbol, titleText: item.data.title)
+                            case SettingGeneral.accentColor:
+                                NavigationLink(value: item) {
+                                    CellView(icon: item.data.sfSymbol, title: item.data.title, detailText: "Demo")
+                                }
+                            default:
+                                NavigationLink(value: item) {
+                                    CellView(icon: item.data.sfSymbol, title: item.data.title, detailText: "Demo")
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-        .navigationTitle(Constants.settingsTitle)
-        .navigationDestination(for: SettingItem.self) { item in
-            BaseWrapperView(for: item.data)
+            .navigationTitle(Constants.settingsTitle)
+            .navigationDestination(for: SettingItem.self) { item in
+                BaseWrapperView(for: item.data)
+            }
         }
     }
 }
@@ -50,22 +54,22 @@ fileprivate struct General_ColorSchemeView: View {
             Image(systemName: sfSymbol)
                 .font(.body)
                 .frame(width: 24, height: 24)
-                .foregroundStyle(themeManager.accentColor)
+                .foregroundStyle(Color.accentColor)
 
             Text(titleText)
 
             Spacer(minLength: 8)
-            Picker("", selection: $themeManager.colorScheme) {
-                ForEach(General_ColorSchemeModel.allCases, id: \.self) {
-                    Text($0.title)
-                        .tag($0.rawValue)
-                        .font(.callout)
-                        .foregroundStyle(.gray)
-                }
-
-            }
-            .pickerStyle(.navigationLink)
-            .padding(.vertical, -12)
+//            Picker("", selection: $themeManager.colorScheme) {
+//                ForEach(General_ColorSchemeModel.allCases, id: \.self) {
+//                    Text($0.title)
+//                        .tag($0.rawValue)
+//                        .font(.callout)
+//                        .foregroundStyle(.gray)
+//                }
+//
+//            }
+//            .pickerStyle(.navigationLink)
+//            .padding(.vertical, -12)
         }
     }
 }
